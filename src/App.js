67cbfa2e26200ef16f836ffeb48438349d098ad7
Vasil Pavlov <vasilpavlov@Vasils-MacBook-Pro.local>
2019-09-20 09:06:40 +0300

    Add sign-up-page branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import EmailAndProfilePage from './components/EmailAndProfilePage';
+//import EmailAndProfilePage from './components/EmailAndProfilePage';
 //import FriendsPage from './components/FriendsPage';
 //import LandingPage from './components/LandingPage';
-//import SignUpPage from './components/SignUpPage';
+import SignUpPage from './components/SignUpPage';
 
 function App() {
 	return (
 		<div className="App">
-			<EmailAndProfilePage />
+			{/* <EmailAndProfilePage /> */}
 			{/* <FriendsPage /> */}
 			{/* <LandingPage /> */}
-			{/* <SignUpPage /> */}
+			<SignUpPage />
 		</div>
 	);
 }
