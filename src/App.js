f8a237b025e46073c244e9753af1ab0ef5489d02
Boyan Donchev <boyandonchev@Boyans-iMac-5.local>
2019-09-20 11:13:20 +0300

    Create stylesheet for App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import FriendsPage from './components/FriendsPage';
 // import LandingPage from './components/LandingPage';
 // import SignUpPage from './components/SignUpPage';
+import '../src/styles/App.css';
 
 function App() {
 	return (
--- src/components/FriendsPage.js
+++ src/components/FriendsPage.js
@@ -16,6 +16,7 @@ const customStyles = {
 	}
 };
 
+
 Modal.setAppElement('#root');
 
 class FriendsPage extends React.Component {
