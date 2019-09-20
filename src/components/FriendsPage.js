6e5ba4b3396c2f1feca68fa85817c32df33f14a6
Boyan Donchev <boyandonchev@Boyans-iMac-5.local>
2019-09-20 10:38:06 +0300

    Put content in modal

--- src/components/FriendsPage.js
+++ src/components/FriendsPage.js
@@ -2,45 +2,98 @@ import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import '../styles/FriendsPage.css';
+import Modal from 'react-modal';
 
-const FriendsPage = () => {
-	return (
-		<Tabs className="tabs">
-			<TabList>
-				<Tab>PROFILE INFO</Tab>
-				<Tab>FRIENDS</Tab>
-				<Tab>ADD VIDEO</Tab>
-			</TabList>
+const customStyles = {
+	content : {
+		top                   : '50%',
+		left                  : '50%',
+		right                 : 'auto',
+		bottom                : 'auto',
+		//marginRight           : '-50%',
+		transform             : 'translate(-50%, -50%)'
 
-			<TabPanel>
-				<h2>Any content 1</h2>
-			</TabPanel>
-
-			<TabPanel>
-				<h2>
-					<div className="intro">
-						<span>Username, now let's get you connected with friends.</span>
-					</div>
-					<div className="subIntro">
-						<span>Find your friends.</span>
-					</div>
-					<div>
-						<button>google contacts</button>
-					</div>
-					<div>
-						<button>facebook contacts</button>
-					</div>
-					<div>
-						<button className="skip">skip this step</button>
-					</div>
-				</h2>
-			</TabPanel>
-
-			<TabPanel>
-				<h2>Any content 2</h2>
-			</TabPanel>
-		</Tabs>
-	);
+	}
 };
+
+Modal.setAppElement('#root');
+
+class FriendsPage extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			modalIsOpen: false
+		};
+
+		this.openModal = this.openModal.bind(this);
+		this.afterOpenModal = this.afterOpenModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	}
+
+	openModal() {
+		this.setState({modalIsOpen: true});
+	}
+
+	afterOpenModal() {
+		// references are now sync'd and can be accessed.
+		//this.subtitle.style.color = '#f00';
+	}
+
+	closeModal() {
+		this.setState({modalIsOpen: false});
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.openModal}>SIGN UP</button>
+				<Modal
+					isOpen={this.state.modalIsOpen}
+					onAfterOpen={this.afterOpenModal}
+					onRequestClose={this.closeModal}
+					style={customStyles}
+					//contentLabel="Example Modal"
+				>
+					<Tabs className="tabs">
+						<TabList>
+							<Tab>PROFILE INFO</Tab>
+							<Tab>FRIENDS</Tab>
+							<Tab>ADD VIDEO</Tab>
+						</TabList>
+
+						<TabPanel>
+							<h2>Any content 1</h2>
+						</TabPanel>
+
+						<TabPanel>
+							<h2>
+								<div className="intro">
+									<span>Username, now let's get you connected with friends.</span>
+								</div>
+								<div className="subIntro">
+									<span>Find your friends.</span>
+								</div>
+								<div>
+									<button>google contacts</button>
+								</div>
+								<div>
+									<button>facebook contacts</button>
+								</div>
+								<div>
+									<button className="skip">skip this step</button>
+								</div>
+							</h2>
+						</TabPanel>
+
+						<TabPanel>
+							<h2>Any content 2</h2>
+						</TabPanel>
+					</Tabs>
+				</Modal>
+			</div>
+		);
+	}
+}
 
 export default FriendsPage;
