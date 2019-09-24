9ad36055d75cb9eb720ebb9d0ad7e20d61e32673
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2019-09-24 09:45:11 +0300

    Created Modal

--- src/components/EmailAndProfilePage.js
+++ src/components/EmailAndProfilePage.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import Modal from 'react-modal';
+import '../styles/EmailAndProfilePage.css';
 
-const EmailAndProfilePage = () => {
-	return <div>Email And ProfilePage</div>;
-};
+Modal.setAppElement("#root");
+
+class EmailAndProfilePage extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {modalIsOpened: false};
+		
+		this.openModal = this.openModal.bind(this);
+	}
+
+	openModal() {
+		this.setState({modalIsOpened: true});
+	}
+
+	render() {
+		return (
+			<div>
+				<button 
+					onClick = {this.openModal}
+					className = "sign-up"
+				>Sign Up</button>
+				<Modal
+					isOpen = {this.state.modalIsOpened}
+					contentLabel = "Confirmation Email and Profile Modal"
+					overlayClassName = "overlay"
+					className = "modal"
+				>
+				<h1>Hello you opened me!</h1>
+				<button>tab</button>
+				<button>is</button>
+				<button>here</button>
+				</Modal>
+			</div>
+		);
+	}
+}
+
 export default EmailAndProfilePage;
-
