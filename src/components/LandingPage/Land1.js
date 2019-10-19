0c2663c3b7c9725f8fd45b649abe7769af2455fa
boqnd <boyandon@gmail.com>
2019-10-19 12:02:54 +0300

    Browse genres

--- src/components/LandingPage/Land1.js
+++ src/components/LandingPage/Land1.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../../styles/LandingPage/Land1.css';
+import genreBtn from '../../imgs/GenreBtn.png';
+
 
 const LandingPage = () => {
     return (
@@ -8,9 +10,22 @@ const LandingPage = () => {
             <div className="firstSection">
                 <div id="header">
                     <span id = "sbsButton"><button id="subscribeBtn"><span id="subscribeTxt">SUBSCRIBE</span></button></span>
+                    <span id = "loginButton"><button id="loginBtn"><span id="loginTxt">Log in</span></button></span>
+                    <span id = "loginButton"><button id="loginBtn"><span id="loginTxt">Sign Up</span></button></span>
                 </div>
                 <div id="text"><p>WATCH MUSIC VIDEOS FOR FREE OR SUBSCRIBE TO ADDITIONAL GENRES.</p></div>
                 <div><button id="signUpBtn"><span id="signUpTxt">SIGN UP FREE</span></button></div>
+            </div>
+            <div className="browseGenresSection">
+                <div id="text"><p>BROWSE GENRES</p></div>
+                <div id="buttons">
+                    <button className="genreBtn"> <img src={genreBtn} alt="genre" className = "genreImage"/></button>
+                    <button className="genreBtn"> <img src={genreBtn} alt="genre" className = "genreImage"/></button>
+                    <button className="genreBtn"> <img src={genreBtn} alt="genre" className = "genreImage"/></button>
+                    <button className="genreBtn"> <img src={genreBtn} alt="genre" className = "genreImage"/></button>
+                    <button className="genreBtn"> <img src={genreBtn} alt="genre" className = "genreImage"/></button>
+                    <button className="genreBtn"> <img src={genreBtn} alt="genre" className = "genreImage"/></button>
+                </div>
             </div>
         </div>
     );
