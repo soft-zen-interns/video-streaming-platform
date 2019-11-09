889b8999207f803492726588e807242c29919ae7
boqnd <boyandon@gmail.com>
2019-11-09 15:08:07 +0200

    Database config update

--- Server/DatabaseConfig/migrations/20191108132151-create-accounts.js
+++ Server/DatabaseConfig/migrations/20191108132151-create-accounts.js
@@ -13,14 +13,6 @@ module.exports = {
       },
       password: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
