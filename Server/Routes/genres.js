f6d5bd45ffb6fd60211886e246f6f042579cada2
boqnd <boyandon@gmail.com>
2019-11-10 20:57:22 +0200

    /genres inserts initial genres in the database

--- Server/Routes/genres.js
+++ Server/Routes/genres.js
@@ -23,11 +23,41 @@ const genres = sequelize.define('genres', {
     title: Sequelize.STRING
 });
 
-router.get('/',function (req, res) {
+router.get('/',function (req, res)
+{
     console.log("Index page loaded.");
     res.send("GO TO \"localhost:8000/genres/all-genres\" TO GET THE GENRES! " +
         "OR " +
         "GO TO \"localhost:8000/genres/create-genre/\" + 'genre name' TO CREATE GENRE!");
+
+    connection.getConnection(function (err, connection)
+    {
+        var pop = "pop";
+        var hiphop = "hip-hop";
+        var rock = "rock";
+        var rap = "rap";
+        var blues = "blues";
+        var genreNames = [pop,hiphop,rock,rap,blues];
+
+        genreNames.forEach(
+            function (name)
+            {
+                connection.query("Select (title) from genres where title = '" + name + "'", function (err, result) {
+                    if (err) {
+                        console.log("Database error");
+                    }
+                    else if(result.toString() !== "")
+                    {
+                        console.log("-> " + name + " already exists.");
+                    }
+                    else {
+                        genres.create({title: name});
+                        console.log("-> " + name + " created successfully");
+                    }
+                })
+            }
+        );
+    });
 });
 
 
@@ -45,19 +75,6 @@ router.get('/all-genres', function (req,res) {
             }
         });
     });
-
-
-    // connection.connect();
-    // connection.query("SELECT * FROM genres ORDER BY id ASC", function (err, result) {
-    //     if (err) {
-	// 		res.send(err.message);
-    //         console.log("Database error!");
-    //     }
-    //     else {
-    //         console.log(result);
-    //         res.send(result);
-    //     }
-    // });
 });
 
 router.get('/create-genre/:genreName', function (req, res) {
@@ -74,12 +91,5 @@ router.get('/create-genre/:genreName', function (req, res) {
         }
     });
 });
-
-// hardcoded genres for testing
-genres.create({title: "pop"});
-genres.create({title: "rock"});
-genres.create({title: "hip-hop"});
-genres.create({title: "r&b"});
-genres.create({title: "country"});
 
 module.exports = router;
