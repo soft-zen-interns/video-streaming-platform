7314865f1c3201e7dfbc5edb4f91144973fa74a3
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2019-11-10 21:19:08 +0200

    Structure Update

--- src/components/GenresSection/GenresRow.js
+++ src/components/GenresSection/GenresRow.js
@@ -1,15 +1,16 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Button} from "react-bootstrap";
 import "../../styles/GenresSection/GenresSection.css";
 
 function GenreRow(props) {
 	return (
 		<Container>
       <Row>
+        {/* might use <Col> */}
         {props.genres.map((genre) => (
-          <Col className="genre-box">
+          <div className="genre-box">
 			      <p>{genre.title.toUpperCase()}</p>
-		      </Col>
+		      </div>
         ))}
       </Row>
     </Container>
