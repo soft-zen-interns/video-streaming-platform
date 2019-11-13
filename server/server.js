90fe2a7291cb302dd02056e2e467a4e5f71483e6
boyandonchev <boyandon@gmail.com>
2019-11-13 11:02:38 +0200

    Initialise profiles-information project

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const genresRoute = require('./Routes/genres.js');
+const profilesRoute = require('./Routes/profiles.js');
 
 app.use('/genres', genresRoute);
+app.use('/profiles', profilesRoute);
+
 
 var server = app.listen(8000, '0.0.0.0',function() {
-    console.log("Server started on port 8000...");
+    console.log("server started on port 8000...");
 });
